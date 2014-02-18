37e75892cc3ac905a064d341920d50bec35cfb6f
Dan <dmiller1199@yahoo.com>
2014-02-18 11:52:45 -0600

    Create constants.hpp

---
+++ constants.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace Constants
+{
+    long double DegreesToRadians = 0.0174532925;
+    long double RadiansToDegrees = 57.2957795;
+    long double GravConstant = 0.00667384;
+
+}
+
+#endif // CONSTANTS_HPP_INCLUDED
